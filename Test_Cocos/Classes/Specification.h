47384ddf7feeea659059bf11103ce01f856d914f
IngwarSV <58367408+IngwarSV@users.noreply.github.com>
2020-07-14 18:49:24 +0300

    Update Specification.h

--- Test_Cocos/Classes/Specification.h
+++ Test_Cocos/Classes/Specification.h
@@ -4,17 +4,21 @@
 #include <string>
 
 
-namespace DEF_SETT {
-	// Main fonts
-	const std::string FONT{ "fonts/arial.ttf" };
 
-	// Actions duration
-	constexpr float DURATION = 2.0f;
-	
-	// Labels' font size and distance between
-	constexpr float FONT_SIZE = 18.0f;
-	
-	// Labels color
-	const cocos2d::Color3B METALLIC_GOLD{ 212, 175, 55 };
-	const cocos2d::Color3B VIOLET{ 131, 55, 127 };
+
+
+
+namespace DEF_SETT {
+	// Ratio
+	const float cHalf = 0.5f;
+	const float cOneThird = 1.0f / 3.0f;
+	const float cTwoThird = 2.0f / 3.0f;
+	const float cOneQuaters = 1.0f / 4.0f;
+	const float cThreeQuaters = 3.0f / 4.0f;
+	const float cOneSixth = 1.0f / 6.0f;
+	const float cFullOpacity = 255.0f;
+	const float cFullSize = 1.0f;
+	const float cOnePixel = 1.0f;
+
+	const int cVerticesAmount = 4;
 }
