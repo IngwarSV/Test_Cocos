d707a40ea4c26e3445b332876b064868311a47f0
IngwarSV <58367408+IngwarSV@users.noreply.github.com>
2020-07-14 18:48:50 +0300

    Update HelloWorldScene.h

--- Test_Cocos/Classes/HelloWorldScene.h
+++ Test_Cocos/Classes/HelloWorldScene.h
@@ -1,66 +1,93 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
 #include "ui/CocosGUI.h"
+#include "../cocos2d/external/json/document.h"
+#include "../cocos2d/external/json/filestream.h"
+#include "../cocos2d/external/json/stringbuffer.h"
 
 #include <array>
+#include <string>
 
+#include "Specifications.h"
+
+enum class eBtnMode {
+	INIT_MODE = 0,
+	FIRST_MODE,
+	SECOND_MODE,
+};
+
+enum class eSpriteVertices {
+	BOTT_LEFT = 0,
+	UP_LEFT,
+	UP_RIGHT,
+	BOTT_RIGHT,
+};
 
 class HelloWorld : public cocos2d::Scene
 {
 private:
-    cocos2d::Sprite* _sprite1 = nullptr;
-    cocos2d::Sprite* _sprite2 = nullptr;
-    cocos2d::Sprite* _sprite3 = nullptr;
-    
-    cocos2d::Sprite* _spriteToCollide = nullptr;
-    
-    cocos2d::Vector<cocos2d::Sprite*> _v_sprites = cocos2d::Vector<cocos2d::Sprite*>(2);
-    
-public:
-    static cocos2d::Scene* createScene();
-    
-    virtual bool init();
-    
-    void on1ButtonPressed(cocos2d::Ref* sender, cocos2d::ui::Widget::TouchEventType type);
-    void on2ButtonPressed(cocos2d::Ref* sender, cocos2d::ui::Widget::TouchEventType type);
-    void on3ButtonPressed(cocos2d::Ref* sender, cocos2d::ui::Widget::TouchEventType type);
-    void onQuitButtonPressed(cocos2d::Ref* sender, cocos2d::ui::Widget::TouchEventType type);
-    
-    bool on3SpriteTouchBegan(cocos2d::Touch*, cocos2d::Event*);
-    void on3SpriteTouchMoved(cocos2d::Touch*, cocos2d::Event*);
-    void on3SpriteTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+	// Sprites
+	cocos2d::Vector<cocos2d::Sprite*> mStaticSprites = cocos2d::Vector<cocos2d::Sprite*>(2);
+	cocos2d::Sprite* mSprite3;
+	cocos2d::Sprite* mPreviousCollidedSprite;
 
-    bool calculateCollision(cocos2d::Sprite* spriteToMove, cocos2d::Sprite* spriteToCheck, cocos2d::Vec2& newPosition,
-        const std::array<cocos2d::Vec2, 4>& spriteToMoveVerts, cocos2d::Vec2 delta, int prominentVertex);
-    cocos2d::Vec2 checkCollision(cocos2d::Sprite* spriteToMove, cocos2d::Vec2 newPosition);
-    
-    CREATE_FUNC(HelloWorld);
+	// Buttons
+	cocos2d::ui::Button* mButton1;
+	cocos2d::ui::Button* mButton2;
+	cocos2d::ui::Button* mButton3;
+
+	// Sprites vertices 
+	std::array<cocos2d::Vec2, 4> mSprite1VertsArr;
+	std::array<cocos2d::Vec2, 4> mSprite2VertsArr;
+	std::array<cocos2d::Vec2, 4> mSprite3VertsArr;
+
+	// Windows size
+	cocos2d::Size mWinSize;
+	// Sprite size
+	cocos2d::Size mSpriteSize;
+	// Actions duration
+	float mActionDuration;
+	// Labels' font size
+	cocos2d::Color3B mVioletColor;
+
+	// Coordinates of zone for additional position calculations
+	float mDangerZoneMinX;
+	float mDangerZoneMaxX;
+	float mDangerZoneMinY;
+	float mDangerZoneMaxY;
+
+	// Buttons' click numbers
+	eBtnMode mBTN1Mode;
+	eBtnMode mBTN2Mode;
+	eBtnMode mBTN3Mode;
+	
+	// ctor
+	HelloWorld();
+
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	void onButtonPressed(cocos2d::Ref* aSender, cocos2d::ui::Widget::TouchEventType aType);
+
+	void on1ButtonPressed(cocos2d::Ref* aSender, cocos2d::ui::Widget::TouchEventType aType);
+	void on2ButtonPressed(cocos2d::Ref* aSender, cocos2d::ui::Widget::TouchEventType aType);
+	void on3ButtonPressed(cocos2d::Ref* aSender, cocos2d::ui::Widget::TouchEventType aType);
+	void onQuitButtonPressed(cocos2d::Ref* aSender, cocos2d::ui::Widget::TouchEventType aType);
+
+	bool on3SpriteTouchBegan(cocos2d::Touch* aTouch, cocos2d::Event* aEvent);
+	void on3SpriteTouchMoved(cocos2d::Touch* aTouch, cocos2d::Event* aEvent);
+	void on3SpriteTouchEnded(cocos2d::Touch* aTouch, cocos2d::Event* aEvent);
+
+	bool is3SpriteInDangerZone(cocos2d::Vec2 currentSprite3Position, cocos2d::Vec2 possibleSprite3Position);
+	bool processCollision(cocos2d::Sprite* aStaticSprite, cocos2d::Vec2& aPossibleSpritePosition, 
+		cocos2d::Vec2 aMoveVector, int aProminentVertex);
+	
+
+	CREATE_FUNC(HelloWorld);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
